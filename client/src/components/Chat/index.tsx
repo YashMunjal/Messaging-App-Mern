546761cec6fa752b26c3fad0d4febe8134f367ed
YashMunjal <yashmu45@gmail.com>
2021-03-09 22:57:12 +0530

    chat

--- client/src/components/Chat/index.tsx
+++ client/src/components/Chat/index.tsx
@@ -1,15 +1,41 @@
-import React, { useEffect } from 'react';
+import { Button, TextField } from "@material-ui/core";
+import React, { useEffect, useState } from "react";
 
 export default function Chat() {
-    useEffect(() =>{
-        const ws =new WebSocket('ws://localhost:1338')
-        ws.addEventListener('open',()=>{
-            ws.send(JSON.stringify({status:'ok'}))
-        },{once:true})
+    const [chatMessage,setChatMessage]=useState('');
+    const [wsRef,setWsRef]=useState<null|WebSocket>(null);
+
+    function sendMessage(){
+        if(wsRef?.readyState!==WebSocket.OPEN) {
+            return;
+        }
+
+        wsRef.send(JSON.stringify(chatMessage));
+    }
 
 
-    })
+  useEffect(() => {
+    const ws = new WebSocket("ws://localhost:1338");
+    ws.addEventListener(
+      "open",
+      () => {
+        ws.send(JSON.stringify({ status: "ok" }));
+      },
+      { once: true }
+    );
+    setWsRef(ws);
 
+    return ()=>{
+        ws.close();
+    }
+  }, []);
 
-    return <h1>Chat Page</h1>
+  return(
+  <div>
+    
+    <h1>Chat Page</h1>
+    <TextField value={chatMessage} onChange={e=>setChatMessage(e.target.value)} variant="outlined" color="primary"></TextField>
+    <Button variant="outlined" color="primary" onClick={sendMessage}>Send Message</Button>
+  </div>
+  )
 }
